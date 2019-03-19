35a3971c450ac1232f5b3b7eb1ae302a311ac4c7
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-19 13:44:04 -0400

    Created POST and first GET async endpoints

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,30 +1,62 @@
 // Important imports
-const express = require('express');
+const express = require("express");
 
 // Custom imports
-const db = require('../data/db.js');
+const db = require("../data/db.js");
 
 // etc.
 const router = express.Router();
 
 // Endpoints: Handle all URLs beginning with /api/posts
-router.post('/', (req, res) => {
-  res.send('POST request to the homepage')
-})
 
-router.get('/', (req, res) => {
-  res.send('Hello from the posts router!')
-})
+// Creates a post using the information sent inside the `request body`.
+router.post("/", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.contents) {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+      const newUserId = await db.insert(req.body);
+      res.status(200).json({...newUserId, ...req.body})
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "There was an error while saving the post to the database" });
+  }
+});
 
-router.get('/:id', (req, res) => {
-  res.send(req.params.id)
-})
+// Returns an array of all the post objects contained in the database.
+router.get("/", async (req, res) => {
+  try {
+    const posts = await db.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "The posts information could not be retrieved." });
+  }
+});
 
-router.delete('/:id', (req, res) => {
-  res.send(req.params.bookId)
-})
+// Returns the post object with the specified id.
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
 
-router.delete('/:id', (req, res) => {
-  res.send(req.params.bookId)
-})
+// Removes the post with the specified id and returns the **deleted post object**. You may need to make additional calls to the database in order to satisfy this requirement.
+router.delete("/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+// Updates the post with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**.
+router.delete("/:id", (req, res) => {
+  res.send(req.params.id);
+});
 module.exports = router;
+
+// - `find()`: calling find returns a promise that resolves to an array of all the `posts` contained in the database.
+// - `findById()`: this method expects an `id` as it's only parameter and returns the post corresponding to the `id` provided or an empty array if no post with that `id` is found.
+// - `insert()`: calling insert passing it a `post` object will add it to the database and return an object with the `id` of the inserted post. The object looks like this: `{ id: 123 }`.
+// - `update()`: accepts two arguments, the first is the `id` of the post to update and the second is an object with the `changes` to apply. It returns the count of updated records. If the count is 1 it means the record was updated correctly.
+// - `remove()`: the remove method accepts an `id` as it's first parameter and upon successfully deleting the post from the database it returns the number of records deleted.
--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-// The `db.js` publishes the following methods:
-
-// - `find()`: calling find returns a promise that resolves to an array of all the `posts` contained in the database.
-// - `findById()`: this method expects an `id` as it's only parameter and returns the post corresponding to the `id` provided or an empty array if no post with that `id` is found.
-// - `insert()`: calling insert passing it a `post` object will add it to the database and return an object with the `id` of the inserted post. The object looks like this: `{ id: 123 }`.
-// - `update()`: accepts two arguments, the first is the `id` of the post to update and the second is an object with the `changes` to apply. It returns the count of updated records. If the count is 1 it means the record was updated correctly.
-// - `remove()`: the remove method accepts an `id` as it's first parameter and upon successfully deleting the post from the database it returns the number of records deleted.
 const express = require('express');
 
 const postsRouter = require('./posts/posts-router')
 
 const server = express();
+server.use(express.json());
 
-// Endpoints:
 server.get('/', (req, res) => {
-  res.send('Hello from the main directory!')
+  res.send('Hello from the WEB API II Challenge server!')
 })
 
 server.use('/api/posts', postsRouter)
