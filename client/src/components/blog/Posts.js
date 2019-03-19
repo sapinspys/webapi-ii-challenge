d6f659c19d7ad7d166c8c56dadc56ee89f87c114
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-19 18:20:01 -0400

    Posts now render on app

---
+++ client/src/components/blog/Posts.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getAllPosts } from '../../actions/actions';
+
+import './Posts.css';
+
+class Posts extends Component {
+    componentDidMount() {
+      this.props.getAllPosts();
+    };
+
+    render() {
+      return (
+        <div className="posts">
+          {this.props.posts.map(post => {
+            return (
+              <div className="post">
+              <p>Author: {post.contents}</p>
+              <p>{post.title}</p>
+              <p>Created: {post.created_at}</p>
+            </div>
+            )
+          })}
+        </div>
+      );
+    }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+    gettingPosts: state.gettingPosts,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps, { getAllPosts })(Posts);
