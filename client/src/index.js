0c164516faf49911ceaee80458c49caa3d2c17f2
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-19 17:31:21 -0400

    Set up application index with necessary imports

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from 'react-redux'
+
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, logger),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
