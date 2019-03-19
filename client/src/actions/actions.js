28614328dd318c4e5eb51c59da9a47a4d3cb309a
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-19 17:34:39 -0400

    Installed axios and built action types and creator

---
+++ client/src/actions/actions.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const GET_POSTS = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
+export const getAllPosts = () => dispatch => {
+  dispatch ({ type: GET_POSTS });
+  axios
+    .get('http://localhost:5000/api/users')
+    .then(res => {
+      dispatch({ 
+        type: SUCCESS, 
+        payload: res.data
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: FAILURE,
+        payload: err.response
+      })
+    })
+}
