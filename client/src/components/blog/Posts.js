832e4bd907c2697c79509194533162d3048822ee
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-20 11:45:58 -0400

    Added custom middleware for non-existant pages

--- client/src/components/blog/Posts.js
+++ client/src/components/blog/Posts.js
@@ -1,38 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import { getAllPosts } from '../../actions/actions';
+import { getAllPosts } from "../../actions/actions";
 
-import './Posts.css';
+import "./Posts.css";
 
 class Posts extends Component {
-    componentDidMount() {
-      this.props.getAllPosts();
-    };
+  componentDidMount() {
+    this.props.getAllPosts();
+  }
 
-    render() {
-      return (
-        <div className="posts">
-          {this.props.posts.map(post => {
-            return (
-              <div key={post.id}className="post">
+  render() {
+    return (
+      <div className="posts">
+        {this.props.posts.map(post => {
+          return (
+            <div key={post.id} className="post">
               <p>{post.contents}</p>
               <p>{post.title}</p>
               <p>{post.created_at}</p>
             </div>
-            )
-          })}
-        </div>
-      );
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     posts: state.posts,
     gettingPosts: state.gettingPosts,
     error: state.error
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, { getAllPosts })(Posts);
+export default connect(
+  mapStateToProps,
+  { getAllPosts }
+)(Posts);
--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const postsRouter = require('./posts/posts-router')
 const cors = require('cors');
 
 const server = express();
+
+// BUILT-IN MIDDLEWARE
 server.use(express.json());
+
+// THIRD PARTY MIDDLEWARE
 server.use(cors());
 
 server.get('/', (req, res) => {
   res.send('Hello from the WEB API II Challenge server!')
 })
 
+// ROUTE HANDLERS ARE ALSO MIDDLEWARE
 server.use('/api/posts', postsRouter)
+
+// CUSTOM MIDDLEWARE! 
+server.use(function(req, res) {
+  res.status(404).send("Ain't nobody got time for that!");
+});
 
 module.exports = server;
